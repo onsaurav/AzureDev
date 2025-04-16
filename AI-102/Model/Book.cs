cdc30b9e44702fae54744df742d0f85f766ac114
Kartik Chandra Biswas <Kartik.biswas@itmagnet.com.au>
2025-04-16 22:55:33 +0600

    Add project files.

---
+++ AI-102/Model/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace AI_102.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publication { get; set; }
+        public DateOnly PublicationDate { get; set; }
+        public string Language { get; set; }
+        public double Price { get; set; }
+    }
+}
